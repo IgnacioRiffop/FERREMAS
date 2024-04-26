b1c936814b9f39c498b57cd51d850acd070168c7
IgnacioRiffop <ig.riffo@duocuc.cl>
2024-04-25 22:16:34 -0400

    Base de datos, url, settings

--- FERREMAS/urls.py
+++ FERREMAS/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_title = "FERREMAS"
+admin.site.site_header = "Administracion FERREMAS"
+admin.site.index_title = "Modulo de administracion"
---
+++ core/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from.views import *
+from rest_framework import routers
+
+# CREAMOS LAS RUTAS DEL API
+router = routers.DefaultRouter()
+
+## SE VAN A CREAR TODAS LAS URLS
+urlpatterns = [
+    #API
+    path('api/', include(router.urls)),
+    #RUTAS
+    path('', index, name="index"),
+  
+]
