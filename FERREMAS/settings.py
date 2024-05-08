9f3c4a0f3682ca7d7ebd28a406724d16cd0b43d6
IgnacioRiffop <ig.riffo@duocuc.cl>
2024-05-08 13:30:53 -0400

    recuperar contraseña listo

--- FERREMAS/settings.py
+++ FERREMAS/settings.py
@@ -152,3 +152,11 @@ MESSAGE_STORAGE = "django.contrib.messages.storage.cookie.CookieStorage"
 #CONFIGURANDO LAS RUTAS DEL LOGIN
 LOGIN_REDIRECT_URL='/'
 LOGOUT_REDIRECT_URL='/'
+
+#CONFIGURANDO SISTEMA EMAIL PARA RECUPERAR CONTRASEÑA
+EMAIL_USE_TLS = True
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "ferremas2024@gmail.com"
+EMAIL_HOST_PASSWORD = "eraq wukb nnbp yytw"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
