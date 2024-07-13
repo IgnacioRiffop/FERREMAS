6c872e9499fedf8a14d7035dd5084bf0e30b497a
fvera21 <fr.verag@duocuc.cl>
2024-07-13 15:27:16 -0400

    campos de registro obligatorios

--- core/forms.py
+++ core/forms.py
@@ -1,12 +1,22 @@
 from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm  
+from django.contrib.auth.models import User
 from .models import *
 
 class RegistroForm(UserCreationForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+    
+    def __init__(self, *args, **kwargs):
+        super(RegistroForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
+        self.fields['email'].required = True
 
 class CantidadForm (ModelForm):
     cantidad = forms.IntegerField(min_value=1 ,widget=forms.NumberInput(attrs={"placeholder":"Ingrese Cantidad"}))
